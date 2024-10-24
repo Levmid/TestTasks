90d676a2f067fa7e346dd29180b74e2ed7aab922
Dmitriy-Gusev <didi200345@gmail.com>
2024-10-25 00:25:17 +0300

    First

--- src/main/java/ru/gusev/spingcourse/exchangeRate/ExchangeRate.java
+++ src/main/java/ru/gusev/spingcourse/exchangeRate/ExchangeRate.java
@@ -2,7 +2,7 @@ package ru.gusev.spingcourse.exchangeRate;
 
 import java.util.Scanner;
 
-public class ExchangeRates {
+public class ExchangeRate {
     private static final double eurRate = 104.51;
     private static final double usdRate = 96.74;
     private static final double gbpRare = 125.26;
